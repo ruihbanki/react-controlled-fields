96f235160a43ecb07bd963923e3090bbd72eba70
Rui Hiroshi Banki <rui@softplan.com.br>
2020-06-16 17:25:38 -0300

    Fix break whne there is no context

--- src/hooks/useField.js
+++ src/hooks/useField.js
@@ -3,7 +3,8 @@ import React from "react";
 import FieldsContext from "../components/FieldsContext";
 
 function useField(name) {
-  const { fields, onChangeFields } = React.useContext(FieldsContext);
+  const context = React.useContext(FieldsContext) || {};
+  const { fields = {}, onChangeFields = () => {} } = context;
 
   const setFieldProp = React.useCallback(
     (prop, value) => {
